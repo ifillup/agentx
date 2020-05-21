771c8797badcfcbe36f40359f1e2706544c6074e
Phillip Willcocks <pwil139@aucklanduni.ac.nz>
2020-05-21 20:58:08 +1200

    player forms

--- components/AddRelationship.js
+++ components/AddRelationship.js
@@ -1,15 +1,28 @@
 import { Button, Form } from 'react-bootstrap';
+import { PlayersContext } from '../context/playersContext/playersState';
+import { useContext } from 'react';
+import { StatementContext } from '../context/statementContext/statementState';
 
 const AddRelationship = () => {
+  const { players } = useContext(PlayersContext);
+  const { transactions } = useContext(StatementContext);
+
   return (
     <Form>
       <Form.Group controlId='formAddPlayer'>
         <Form.Label>Player name</Form.Label>
+
         <Form.Control as='select'>
-          <option>make a dropdown of all a users players</option>
+          {players.map((player) => (
+            <option>{player.name}</option>
+          ))}
         </Form.Control>
         <Form.Control as='select'>
-          <option>make a dropdown of all loaded accounts ID+USERNAME</option>
+          {transactions.map((t) => (
+            <option>
+              club:{t.club} player:{t.playername} id:{t.playerID}
+            </option>
+          ))}
         </Form.Control>
       </Form.Group>
       <Button variant='primary' type='submit'>
